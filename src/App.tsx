81d20c61c9803878e48555671d55563db6eb1a67
Roman Bereziuk <roma.physicist@gmail.com>
2024-09-16 12:41:08 +0300

    Install Chakra UI package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
       <h1>Let's FUN</h1>
+      <Button colorScheme='blue'>Goooooooo</Button>
     </>
   )
 }
