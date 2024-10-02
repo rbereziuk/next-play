67edb79ab0916ad6055fd199cd6b513110a5704f
Roman Bereziuk <roma.physicist@gmail.com>
2024-10-02 13:42:34 +0300

    Fetching the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import './App.css'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
+import { GameGrid } from './components/GameGrid'
 
 function App() {
+  console.log(import.meta.env)
 
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem as="nav" area="nav" bg="lightskyblue">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem as="aside" area="aside" bg="lightgreen">Aside</GridItem>
+        <GridItem as="aside" area="aside" bg="lightgreen">
+          Aside
+        </GridItem>
       </Show>
-      <GridItem as="main" area="main" bg="lightcoral">Main</GridItem>
+      <GridItem as="main" area="main" bg="lightcoral">
+        <GameGrid />
+      </GridItem>
     </Grid>
   )
 }
