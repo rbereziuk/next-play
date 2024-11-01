d49906773b00ed8da7d0775d8604009df4d53a47
Roman Bereziuk <roma.physicist@gmail.com>
2024-11-01 17:57:58 +0200

    Make genres static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
-import { Genre } from '../types/Genre'
-import { useData } from './useData'
+//import { Genre } from '../types/Genre'
+//import { useData } from './useData'
+import genres from '../data/genres'
 
-export const useGenres = () => useData<Genre>('/genres')
+//export const useGenres = () => useData<Genre>('/genres')
+export const useGenres = () => ({ data: genres, isLoading: false, error: null })
