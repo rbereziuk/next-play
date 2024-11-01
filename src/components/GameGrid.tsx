1464f8251481b6d7b835a5e9cab6d02d2bdb5463
Roman Bereziuk <roma.physicist@gmail.com>
2024-11-01 18:24:27 +0200

    Move error UI out from GameGrid markup

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,12 @@ const SKELETON_ITEMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 export const GameGrid = ({ gamesQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gamesQuery)
 
+  if (error) {
+    return <Text>{error}</Text>
+  }
+
   return (
     <Box as="section" p={5} pl={0}>
-      {error && <Text>{error}</Text>}
       <SimpleGrid as="ul" columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {isLoading &&
           SKELETON_ITEMS.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
